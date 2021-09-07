d6a6cf2425207e3c069f387dd8e1fb4ffbece49f
Gustavo Gambarra <gustavo.gambarra@ccc.ufcg.edu.br>
2021-09-07 19:13:49 -0300

    implementing subjects page

--- front/src/pages/Subjects/index.js
+++ front/src/pages/Subjects/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Container, CardsContainer, Header } from "./style";
+import SubjectCard from '../../components/SubjectCard';
+import plusIcon from "../../assets/plus-circle.svg";
 
 function Subjects() {
-  return <h1>ola</h1>;
+  return (
+    <Container>
+      <div style={{}}>
+        <Header>
+        <h1>Subjects</h1>
+        <img 
+        src={plusIcon} 
+        style={{ width: "28px", height: "28px", cursor: "pointer" }}/>
+        </Header>
+      </div>
+      <CardsContainer>
+        <SubjectCard name="Matemática" time="600"/>
+        <SubjectCard name="História" time="600"/>
+      </CardsContainer>
+    </Container>
+  );
 }
 
 export default Subjects;
