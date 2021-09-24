d916ce453dd077a50fd14aaad38e36fb4f08e85d
Gustavo Gambarra <gustavo.gambarra@ccc.ufcg.edu.br>
2021-09-24 00:16:03 -0300

    Subject activities page created

--- front/src/pages/SubjectPage/style.js
+++ front/src/pages/SubjectPage/style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 80%;
+  width: 100%;
   height: 100vh;
   padding: 100px 50px;
   display: flex;
@@ -18,3 +18,23 @@ export const Container = styled.div`
   }
 `;
 
+export const Header = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  h1 {
+    margin-bottom: 60px;
+  }
+
+  h2{
+    margin: 30px 0;
+  }
+`;
+
+export const CardsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  flex-wrap: wrap;
+  gap: 15px;
+`;
